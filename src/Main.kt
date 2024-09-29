5aa203c3121930655d405eb9f0d9410d797a60ce
Ange-Etoile <etoilematjabo@gmail.com>
2024-09-29 12:59:45 +0100

    nouvelle modification

--- src/Main.kt
+++ src/Main.kt
@@ -2,14 +2,35 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val int1 = 3
+    val int2 = 6.8
+//    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+//    // to see how IntelliJ IDEA suggests fixing it.
+//    println("Hello, " + name + "!")
+//
+//    for (i in 1..5) {
+//        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//        println("i = $i")
+//        println("bonjour")
+//    }
+    when(int1){
+        6 -> println("is 6")
+        3 -> println("is 3")
+        4 -> println("is 4")
+    }
+    // avec les ternaires
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+    val res = when(int2){
+        5.0 -> 5.8
+        8.0 -> 7.9
+        else -> 7.6
+
+    }
+    for (i in 1..10 step 2){
         println("i = $i")
-        println("bonjour")
+    }
+    for ((index,value) in (1..10).withIndex()){
+        println("$index : $value")
     }
 }
